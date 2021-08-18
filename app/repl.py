ae568f7261f7aaec9de0bc54f5768f7cd0764c6f
Liu233w <gh@liu233w.com>
2021-08-18 20:23:25 +1000

    Add tools to reload modules

--- app/repl.py
+++ app/repl.py
@@ -10,5 +10,33 @@ import IPython
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(dir_path)
 
+## Codes that handle module reloading (from https://stackoverflow.com/a/61019586)
+
+PRELOADED_MODULES = set()
+
+def init() :
+    # local imports to keep things neat
+    from sys import modules
+    import importlib
+
+    global PRELOADED_MODULES
+
+    # sys and importlib are ignored here too
+    PRELOADED_MODULES = set(modules.values())
+
+def reload() :
+    from sys import modules
+    import importlib
+
+    for module in set(modules.values()) - PRELOADED_MODULES :
+        try :
+            importlib.reload(module)
+        except :
+            # there are some problems that are swept under the rug here
+            pass
+
 if __name__ == '__main__':
-    IPython.start_ipython()
+    init()
+    IPython.start_ipython(user_ns={
+        'reload': reload,
+    })
