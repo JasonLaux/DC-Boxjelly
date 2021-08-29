ec9a3e6ee68612b4ecb201f30b5b4568c8f274aa
johnlky <johnkahyong.low@student.unimelb.edu.au>
2021-08-30 00:15:03 +0800

    Added dynamic search bar

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHeaderView, QTableView, QItemDelegate
+from PyQt5.QtCore import Qt, QSortFilterProxyModel, QAbstractTableModel
 import sys
 from utils import loadUI
 
@@ -22,6 +23,25 @@ class MainWindow(QMainWindow):
         # Return to Client Info Page
         self.ui.returnButton_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.clientInfoPage))
         
+        #Dynamic search
+        calNumber = ["Cal Number1", "Cal Number2", "Cal Number3"] 
+        clientName = ["Adams", "Peter", "James"]
+        dataList = []
+        for i in range(len(calNumber)):
+            dataList.append(['',calNumber[i],clientName[i]])
+
+        self.model = TableModel(dataList)
+        self.proxy_model = QSortFilterProxyModel()
+        self.proxy_model.setFilterKeyColumn(-1) # Search all columns.
+        self.proxy_model.setSourceModel(self.model)
+        self.proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        self.ui.searchBar.textChanged.connect(self.proxy_model.setFilterFixedString)
+        self.ui.homeTable.setModel(self.proxy_model)
+        for i in range(3):
+            self.ui.homeTable.horizontalHeader().setSectionResizeMode(i, QtWidgets.QHeaderView.Stretch) #column stretch to window size
+        self.ui.homeTable.setItemDelegate(AlignDelegate()) # text alignment
+
+
         ## Table insertion
         # self.ui.jobsTable
         # self.ui.equipmentsTable
@@ -145,6 +165,53 @@ class AddEquipmentWindow(QMainWindow):
         else:
             event.ignore() 
 
+class TableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+        self.horizontalHeaders = [''] * 3
+        self.setHeaderData(0, Qt.Horizontal, "SELECT")
+        self.setHeaderData(1, Qt.Horizontal, "CAL NUMBER")
+        self.setHeaderData(2, Qt.Horizontal, "CLIENT NAME")
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            try:
+                return self.horizontalHeaders[section]
+            except:
+                pass
+        return super().headerData(section, orientation, role)
+
+    def setHeaderData(self, section, orientation, data, role=Qt.EditRole):
+        if orientation == Qt.Horizontal and role in (Qt.DisplayRole, Qt.EditRole):
+            try:
+                self.horizontalHeaders[section] = data
+
+                return True
+            except:
+                return False
+        return super().setHeaderData(section, orientation, data, role)
+
+class AlignDelegate(QItemDelegate):
+    def paint(self, painter, option, index):
+        option.displayAlignment = Qt.AlignCenter
+        QItemDelegate.paint(self, painter, option, index)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
