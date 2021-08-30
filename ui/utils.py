519482ce9dec9388f2d7cc4de009670848a277fc
JianhengShi <jianheng@student.unimelb.edu.au>
2021-08-30 23:43:12 +0800

    Add getEquipmentsTableData and getRunsTableData function

--- ui/utils.py
+++ ui/utils.py
@@ -4,7 +4,7 @@ from PyQt5.uic import loadUi
 import sys
 import pandas as pd
 import os
-from app.core.models import Job
+from app.core.models import Job, Equipment
 
 def loadUI(str, window):
     ui_file_name = str
@@ -37,12 +37,47 @@ def getHomeTableData():
         'Client Address': []
     }
     for job in Job:
-        print(job)
         data['status'].append(False),
         data['CAL Number'].append(job._id),
         data['Client Name'].append(job.client_name),
         #data['Clinet Address'].append(str(job.client_address_1)+' '+str(job.client_address_2)),
         data['Client Address'].append(''),
     
+    df = pd.DataFrame(data)
+    return df
+
+'''
+Return equipments data as Dataframe
+'''
+def getEquipmentsTableData(job: Job):
+    #job = Job[jobID]
+    data = {
+        'status': [],
+        'Make/Model': [],
+        'Serial Num': []
+    }
+    for equip in job:
+        data['status'].append(False),
+        data['Make/Model'].append(equip.model),
+        data['Serial Num'].append(equip.serial),
+    df = pd.DataFrame(data)
+    return df
+
+'''
+Return runs data as Dataframe
+'''
+def getRunsTableData(equip: Equipment):
+    #equip = Job[jobID][equipID]
+    data = {
+        'status': [],
+        'ID': [],
+        'Added Time': [],
+        'Edited Time': []
+    }
+    for run in equip.mex:
+        data['status'].append(False),
+        data['ID'].append(run._id),
+        data['Added Time'].append(run.added_at),
+        data['Edited Time'].append(run.edited_at),
     df = pd.DataFrame(data)
     return df
