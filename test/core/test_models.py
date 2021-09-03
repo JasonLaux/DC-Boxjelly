da8d923144c721ca042c416a77721b8f03aba105
Liu233w <gh@liu233w.com>
2021-09-03 16:36:32 +1000

    Fix unittest
    Mock time

--- test/core/test_models.py
+++ test/core/test_models.py
@@ -1,7 +1,10 @@
+import datetime
 from pathlib import Path
 import unittest
 import shutil
 import importlib
+import time_machine
+import pytz
 
 from app.core import definition, models
 
@@ -135,10 +138,12 @@ class TestMexRun(ModelTestBase):
             'data/jobs/CAL0001/AAA_123/MEX/1/meta.ini').read_text().strip().splitlines()
         self.assertEqual(len(lines), 4)
         self.assertEqual(lines[0], '[DEFAULT]')
-        self.assertRegexpMatches(lines[1], r'^added_at = ')
-        self.assertRegexpMatches(lines[2], r'^edited_at = ')
+        self.assertRegex(lines[1], r'^added_at = ')
+        self.assertRegex(lines[2], r'^edited_at = ')
         self.assertEqual(lines[3], 'operator = Random Person')
 
+    @time_machine.travel(datetime.datetime(2021, 1, 1, 12, 0, 0,
+                                           tzinfo=pytz.timezone('Australia/Melbourne')))
     def test_raw_file(self):
         TEST_DATA_FOLDER = Path(__file__).parent / '_assert' / 'Data'
         CLIENT_A_RUN1_CLIENT = TEST_DATA_FOLDER / \
