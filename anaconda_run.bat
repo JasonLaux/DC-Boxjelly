4c40fa339c88ab02293dd85d1ec692a0d449b217
Zavier <ez860421@gmail.com>
2021-10-21 16:35:01 +0800

    remove Redundant file

--- anaconda_run.bat
+++
@@ -1,5 +0,0 @@
-call activate DC
-cd app/gui
-pyrcc5 resources.qrc -o resources.py
-cd ../..
-python -m app.main
--- develop_run.bat
+++
@@ -1,4 +0,0 @@
-cd app/gui
-pipenv run pyrcc5 resources.qrc -o resources.py
-cd ../..
-pipenv run main
